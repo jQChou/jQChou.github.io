a0bddb90b5314a04266290578182048538dae9b1
zjq <467446615@qq.com>
2016-10-12 16:36:11 +0800

    test

--- app.router.js
+++ app.router.js
@@ -1,5 +1,5 @@
 demo.config(["$stateProvider", "$locationProvider", "$urlRouterProvider", function ($stateProvider, $locationProvider, $urlRouterProvider) {
-    $urlRouterProvider.when("/", "/pageIndex");
+    $urlRouterProvider.when("", "/pageIndex");
     $stateProvider.state('pageIndex', {
             url: '/pageIndex',
             templateUrl: 'common/templete/pageIndex.html?rev=4a3e0be9ff',
@@ -16,5 +16,4 @@ demo.config(["$stateProvider", "$locationProvider", "$urlRouterProvider", functi
             templateUrl: 'page2/page2.html?rev=b28fa609a0',
             controller: 'page2Ctrl'
         });
-    $locationProvider.html5Mode(true);
 }])
